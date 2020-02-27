8e786e97aceedc07a004a244caf80b03666339c6
Christopher Ziemacki <christopher.ziemacki@outlook.com>
2020-02-27 09:09:35 +0200

    fun fun

--- Firewatch.Models/Resources/ResourceType.cs
+++ Firewatch.Models/Resources/ResourceType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 
 namespace Firewatch.Models.Resources
@@ -7,21 +8,12 @@ namespace Firewatch.Models.Resources
     public class ResourceType
     {
         public string Name { get; set; }
+
+        public string DisplayName { get; set; }
+
         public string UriTemplate { get; set; }
 
         public IEnumerable<ResourceProperty> Properties { get; set; }
-
-        public ResourceType()
-        {
-
-        }
-
-        public ResourceType(string name, string uriTemplate, IEnumerable<ResourceProperty> resourceProperties)
-        {
-            Name = name;
-            UriTemplate = uriTemplate;
-            Properties = resourceProperties ?? throw new ArgumentNullException(nameof(resourceProperties));
-        }
 
         public override bool Equals(object obj)
         {
